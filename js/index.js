36d4ea4d2e06b0c87a31b22b3710b59e0f9d9de9
Marco Torello <marcotorello@gmail.com>
2016-06-05 16:55:47 +0200

    first release web

--- js/index.js
+++ js/index.js
@@ -1,15 +1,40 @@
+//update counter values
+	function updateCounters(){
+		$.ajax({
+			url: '/webservice/web/counters',
+			method: 'GET',
+			success: function(data){
+				var jsonData = JSON.parse(data);
+				$("#count-day").html(jsonData.day);
+				$("#count-week").html(jsonData.week);
+				$("#count-month").html(jsonData.month);
+			}
+		});
+	}
+
 $(document).ready(function(){
+	
+	updateCounters();
+	
 	$("#play").click(function(){
 		var $this = $(this);
 	    if ($this.data("executing")) return;
 	    $this.data("executing", true);
     	$this.attr("disabled", true);
 		
+		//increment counter
+		$.post('/webservice/web/play',
+			function(result) {
+				// Do something with the result
+			}
+		);
+		
 		var audio = new Audio('audio/sad.mp3');
 		audio.addEventListener("ended", function(){
 		    $('#play').attr('checked', false);
     		$this.removeData("executing");
     		$this.removeAttr("disabled");
+			updateCounters();
 		});
 		audio.play();
 	});	
